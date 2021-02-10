d0cf90bee2668ad5eb26dd16fbb23c36071c984a
Brody Bayley <brodybayley@gmail.com>
2021-02-10 22:34:45 +0000

    changed handleUserInput function and added input.js file

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541
